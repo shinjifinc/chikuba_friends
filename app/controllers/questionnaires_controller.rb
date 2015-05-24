eac79ae46e600fcc57073188a665b49b0eb3bf11
Tsutomu Chikuba <chikuba.tsutomu@gmail.com>
2015-05-24 16:16:13 +0900

    質問組み込み

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,14 +1,16 @@
 class QuestionnairesController < ApplicationController
 
   def index
+    @company_ideas = CompanyIdea.order('rand()').take 4
   end
 
   def form_response
     # params[:stay_id]
     res = { contents: { } }
-    4.times.each do |cnt|
+    company_ideas = CompanyIdea.order('rand()').take 4
+    company_ideas.each_with_index do |company_idea, cnt|
       unless (cnt+1) == params[:stay_id]
-        res[:contents].update("new_data#{cnt+1}" => "#{cnt}")
+        res[:contents].update("new_data#{cnt+1}" => "#{company_idea.idea}")
       end
     end
 
